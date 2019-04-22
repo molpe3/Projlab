b1aed60460c8c74f8ea8454e76f3d25b3251a56e
molpe3 <molnarpeti3@gmail.com>
2019-04-22 16:44:03 +0200

    Update Timer.java

--- src/Timer.java
+++ src/Timer.java
@@ -1,12 +1,36 @@
 import java.util.ArrayList;
+
+/**
+* A Steppable interfészt implementáló objektumok működésének ütemezését végző osztály, lépteti azokat.
+*/
 public class Timer {
 	private ArrayList<Steppable> steppables;
+	
+	/**
+	* Meghívja minden léptethető elemre a Step() metódust.
+	*/
 	public void Tick() {
-		for (Steppable s:steppables){
-			s.Step();
-		}
+		if( steppables != null )
+			for (Steppable s:steppables)
+				s.Step();
 	}
+	
+	/**
+	* Hozzáadja a paraméterként kapott Steppable-t a léptethető objektumokhoz.
+	* @param hozzáadni kívánt Steppable.
+	*/
+	public void AddSteppable(Steppable s) {
+		if( steppables == null )
+			steppables = new ArrayList<Steppable>();
+		steppables.Add(s);
+	}
+	
+	/**
+	* Eltávolítja a paraméterként kapott Steppable-t a léptethető objektumok közül.
+	* @param eltávolítandó Steppable.
+	*/
 	public void RemoveSteppable(Steppable s) {
-		steppables.remove(s);
+		if( steppables != null && !steppables.isEmpty() )
+			steppables.remove(s);
 	}
 }
