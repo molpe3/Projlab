4905653750cd5a45ef9e6387e34769a25c3e5263
TNemes <36825562+TNemes@users.noreply.github.com>
2019-04-22 14:34:57 +0200

    Update ScaredPanda.java

--- src/ScaredPanda.java
+++ src/ScaredPanda.java
@@ -1,33 +1,52 @@
 import java.util.Random;
+
+/**
+* Megijed, ha jÃ¡tÃ©kgÃ©p csilingel mellette. Ilyenkor elengedi a fogott Ã¡llato(ka)t Ã©s felbomlik a sor.
+*/
 public class ScaredPanda extends Panda {
 	public ScaredPanda(String name) {
 		super(name);
 	}
+	
+	/**
+	* Lets go of its puller and pulled if applicable.
+	*/
 	public void GetScared(){
 		if (puller!=null)
 			Disband();	//Ha sorban volt, az felbomlik
 	}
+	
+	/**
+	* Checks whether the calling SlotMachine is on the same or on a neighboring tile.
+	* If yes, it gets scared.
+	* @param ob the observer that called the update
+	*/
 	public void Update(Observable o) {
 		SlotMachine sm=(SlotMachine) o;
 		int side=tile.CompareTile(sm.GetTile());
-		if (side==-1) //ha azonos csempén van a panda és a játékgép
+		if (side==-1) //ha azonos csempÃ©n van a panda Ã©s a jÃ¡tÃ©kgÃ©p
 		{
 			GetScared();
 		}
-		else if (side>0)	//ha szomszédosak
+		else if (side>0)	//ha szomszÃ©dosak
 		{
 			GetScared();
 		}
 	}
+	
+	/**
+	* Prints all relevant information.
+	* This includes name, tile, puller and pulled.
+	*/
 	public void Print()
 	{
 		System.out.println("\t"+this.name);
 		System.out.println("\tCsempe:");
 		System.out.println("\t\t"+tile.GetName());
-		System.out.println("\tHúzó állat:");
+		System.out.println("\tHÃºzÃ³ Ã¡llat:");
 		if (puller!=null)
 			System.out.println("\t\t"+puller.GetName());
-		System.out.println("\tHúzott panda:");
+		System.out.println("\tHÃºzott panda:");
 		if (pulled!=null)
 			System.out.println("\t\t"+pulled.GetName());
 		
