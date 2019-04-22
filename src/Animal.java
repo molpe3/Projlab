0210c3e4ca19da4e2a38a98387d5cb455c267ad8
TNemes <36825562+TNemes@users.noreply.github.com>
2019-04-22 13:42:53 +0200

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,44 +1,94 @@
-//komment teszt
+/**
+* Absztrakt ősosztály az állatoknak.
+*/
 
 public abstract class Animal implements Printable{
 	protected Panda pulled;
 	protected Tile tile;
 	protected String name;
 	
+	/**
+	* Animals die when they fall, calling their Destroy() method
+	*/
 	public void Fall() {
 		Destroy();
 	}
+	
+	/**
+	* removes animal from the tile it occupies
+	* animal is then deleted from the game
+	*/
 	public void Destroy(){
 		tile.RemoveAnimal();
 		Game.getInstance().DeleteAnimal(this);
 	}
+	
+	/**
+	* subclasses implement this functionality
+	*/
 	public void Move(int side){
 		
 	}
+	
+	/**
+	* the default behavior is returning false, thus preventing movement
+	*/
 	public boolean CollideWithPanda(Panda p){
 		return false;
 	}
+	
+	/**
+	* the default behavior is returning false, thus preventing movement
+	*/
 	public boolean CollideWithOrangutan(Orangutan o){
 		return false;
 	}
+	
+	/**
+	* disband cascades through pulled pandas
+	*/
 	public void Disband(){
 		if (pulled!=null){
 			pulled.Disband();
 		}
 	}
+	
+	/**
+	* returns with the tile the animal is occupying
+	* @ret tile the tile the animal is standing on
+	*/
 	public Tile GetTile(){
 		return tile;
 	}
+	
+	/**
+	* returns with pulled panda
+	* @ret pulled the panda this animal is pulling
+	*/
 	public Panda GetPulled(){
 		return pulled;
 	}
+	
+	/**
+	* sets pulled panda 
+	* @param p the panda it's starting to pull
+	*/
 	public void SetPulled(Panda p){
 		pulled=p;
 	}
 	
+	/**
+	* sets occupied tile
+	* @param t the tile the animal's moving to
+	*/
 	public void SetTile(Tile t) {
 		tile=t;
 	}
+	
+	/**
+	* returns with the animal's name
+	* @ret name the name
+	*/
 	public String GetName()
 	{
 		return name;
