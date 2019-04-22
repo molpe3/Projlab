84514bce880ab070e8c9aa4038c7e6ae746b8a66
TNemes <36825562+TNemes@users.noreply.github.com>
2019-04-22 14:38:57 +0200

    Update SlotMachine.java

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,25 +1,51 @@
 import java.util.Random;
+
+/**
+* Csilingel, és értesíti a regisztrált pandákat erről.
+*/
 public class SlotMachine extends Thing {
 	public SlotMachine(String name) {
 		super(name);
 	}
+	
+	/**
+	* Jingles and notifies subscribers of this fact.
+	*/
 	public void Jingle() {
 		Notify();
 	}
+	
+	/**
+	* Jingles on every timer tick.
+	*/
 	public void Step() {
 		Random rand=new Random();
-		if (rand.nextInt(1)<2) {	//most m�g mindig csin�lja, randomiz�l�s megoldani, hogy ki/be kapcsolhat� legyen
+		if (rand.nextInt(1)<2) {	//most még mindig csinálja, randomizálás megoldani, hogy ki/be kapcsolható legyen
 			Jingle();
 		}
 	}
+	
+	/**
+	* Notifies every subscribing observer of the jingling.
+	*/
 	public void Notify() {
 		for (Observer o:observers) {
 			o.Update(this);
 		}
 	}
+	
+	/**
+	* Removes an observer from its list of subscribers.
+	* @param o the observer to be removed
+	*/
 	public void Detach(Observer o) {
 		observers.remove(o);
 	}
+	
+	/**
+	* Prints all relevant information.
+	* This includes name, tile and observers.
+	*/
 	public void Print() {
 		System.out.println("\t"+this.name);
 		System.out.println("\tCsempe:");
