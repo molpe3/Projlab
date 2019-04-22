e9af9d3076243356c5014c04c4d2cc72cfd6242b
TNemes <36825562+TNemes@users.noreply.github.com>
2019-04-22 14:44:39 +0200

    Update Thing.java

--- src/Thing.java
+++ src/Thing.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
+
+/**
+* Ősosztály a pályán lévő tárgyaknak.
+*/
 public abstract class Thing implements Steppable, Observable, Printable{
 	protected Tile tile;
 	protected String name;
@@ -7,18 +11,43 @@ public abstract class Thing implements Steppable, Observable, Printable{
 		this.name=name;
 		observers=new ArrayList<Observer>();
 	}
+	
+	/**
+	* returns with the tile it occupied
+	* @return tile the tile in question
+	*/
 	public Tile GetTile() {
 		return tile;
 	}
+	
+	/**
+	* sets occupied tile
+	* @param tile the tile we want the thing to occupy
+	*/
 	protected void SetTile(Tile tile) {
 		this.tile=tile;
 	}
+	
+	/**
+	* Adds a new observer to its list of subscribers.
+	* @param o the observer to be added
+	*/
 	public void Subscribe(Observer o) {
 		observers.add(o);
 	}
+	
+	/**
+	* Removes a new observer from its list of subscribers.
+	* @param o the observer to be removed
+	*/
 	public void Detach(Observer o) {
 		observers.remove(o);
 	}
+	
+	/**
+	* returns with its own name
+	* @return name the thing's name
+	*/
 	public String GetName()
 	{
 		return name;
